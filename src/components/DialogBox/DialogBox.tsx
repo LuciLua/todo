11bce0479cb24b678d0056bc9bdac8d840c4f1b0
LuciLua <luci.lua81@gmail.com>
2022-07-23 17:53:43 -0300

    :star2: update dialogBox style:font>ubuntu and spacing++

--- src/components/DialogBox/DialogBox.tsx
+++ src/components/DialogBox/DialogBox.tsx
@@ -15,23 +15,23 @@ function DialogBox({ op }) {
         setConfirm(false)
     }, [op])
 
-    function clearOptions(confirmClear:boolean, optionForCloseBox:boolean) {
+    function clearOptions(confirmClear: boolean, optionForCloseBox: boolean) {
         setConfirm(confirmClear)
         setOption(optionForCloseBox)
     }
 
-    if (option == false)  return 
+    if (option == false) return
     else {
         return (
             <div className={styles.fullSizePage}>
                 <div className={styles.box}>
-                    <h1>Apagar tudo ({total})</h1>
+                    <h1>Apagar tudo? ({total})</h1>
                     <p>Voce tem certeza que deseja apagar todos os items da lista?</p>
                     <div className={styles.btnCollection}>
-                        <button onClick={() => clearOptions(true, false)}>
+                        <button onClick={() => clearOptions(true, false)} title="apagar">
                             Apagar
                         </button>
-                        <button onClick={() => clearOptions(false, false)}>
+                        <button onClick={() => clearOptions(false, false)} title="cancelar">
                             Cancelar
                         </button>
                     </div>
